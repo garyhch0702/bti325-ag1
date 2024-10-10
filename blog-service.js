713e554fe65ae06800a4c898ac7bbd8561157ed7
Chenghao Hu <chu34@myseneca.ca>
2024-10-10 16:08:02 -0400

    Initial commit for BTI325 Assignment 2

---
+++ blog-service.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+// Global arrays
+let posts = [];
+let categories = [];
+
+// Initialize function to read the JSON files
+function initialize() {
+  return new Promise((resolve, reject) => {
+    fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+      if (err) {
+        reject('Unable to read posts file');
+      } else {
+        posts = JSON.parse(data);
+
+        fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+          if (err) {
+            reject('Unable to read categories file');
+          } else {
+            categories = JSON.parse(data);
+            resolve();
+          }
+        });
+      }
+    });
+  });
+}
+
+// Function to get all posts
+function getAllPosts() {
+  return new Promise((resolve, reject) => {
+    if (posts.length > 0) {
+      resolve(posts);
+    } else {
+      reject('No results returned');
+    }
+  });
+}
+
+// Function to get only published posts
+function getPublishedPosts() {
+  return new Promise((resolve, reject) => {
+    const publishedPosts = posts.filter(post => post.published === true);
+    if (publishedPosts.length > 0) {
+      resolve(publishedPosts);
+    } else {
+      reject('No results returned');
+    }
+  });
+}
+
+// Function to get all categories
+function getCategories() {
+  return new Promise((resolve, reject) => {
+    if (categories.length > 0) {
+      resolve(categories);
+    } else {
+      reject('No results returned');
+    }
+  });
+}
+
+// Export the functions
+module.exports = {
+  initialize,
+  getAllPosts,
+  getPublishedPosts,
+  getCategories
+};
---
+++ server.js
@@ -0,0 +1,66 @@
+/*********************************************************************************
+* BTI325 – Assignment 02
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Chenghao Hu Student ID: 149773228 Date: [2024/10/10]
+*
+* Online (Vercel) URL: [Insert Your Vercel URL]
+********************************************************************************/
+
+const express = require('express');
+const path = require('path');
+const blogService = require('./blog-service');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files from 'public' folder
+app.use(express.static('public'));
+
+// Redirect root to /about
+app.get('/', (req, res) => {
+  res.redirect('/about');
+});
+
+// Serve the about.html file
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+// Serve the blog posts that are published
+app.get('/blog', (req, res) => {
+  blogService.getPublishedPosts()
+    .then(posts => res.json(posts))
+    .catch(err => res.json({ message: err }));
+});
+
+// Serve all posts
+app.get('/posts', (req, res) => {
+  blogService.getAllPosts()
+    .then(posts => res.json(posts))
+    .catch(err => res.json({ message: err }));
+});
+
+// Serve all categories
+app.get('/categories', (req, res) => {
+  blogService.getCategories()
+    .then(categories => res.json(categories))
+    .catch(err => res.json({ message: err }));
+});
+
+// 404 Page Not Found route
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
+// Initialize blog-service and start the server
+blogService.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Express http server listening on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(`Failed to initialize blog service: ${err}`);
+  });
